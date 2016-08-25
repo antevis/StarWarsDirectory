5b1973a486496089fa4d078cade0c7a27037ce73
antevis <polyconnect@me.com>
2016-08-25 18:25:55 +0300

    UICollectionViewAs entry point

---
+++ StarWarsDirectory/RootDataCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RootDataCollectionViewCell.swift
+//  StarWarsDirectory
+//
+//  Created by Ivan Kazakov on 25/08/16.
+//  Copyright © 2016 Antevis UAB. All rights reserved.
+//
+
+import UIKit
+
+class RootDataCollectionViewCell: UICollectionViewCell {
+    
+}
