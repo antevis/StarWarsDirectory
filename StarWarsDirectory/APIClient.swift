aa9bd29015d9ceb4b3667727196ec6cdf607c4d7
antevis <polyconnect@me.com>
2016-09-14 16:57:45 +0300

    scheduled

--- StarWarsDirectory/APIClient.swift
+++ StarWarsDirectory/APIClient.swift
@@ -2,17 +2,16 @@
 //  APIClient.swift
 //  Stormy
 //
-//  Created by Ivan Kazakov on 09/06/16.
+//  Created by Ivan Kazakov on 10/09/16.
 //  Copyright © 2016 Treehouse. All rights reserved.
 //
 
 import Foundation
 
-public let ANTNetworkingErrorDomain = "lt.antevis.Stormy.NetworkingError"
-
+public let ANTNetworkingErrorDomain = "lt.antevis.NetworkingError"
 public let MissingHTTPResponseError: Int = 10
 public let UnexpectedResponseError: Int = 20
-public let MissingErrorError: Int = 30
+public let AbnormalError: Int = 30
 
 typealias JSON = [String: AnyObject]
 typealias JSONTaskCompletion = (JSON?, NSHTTPURLResponse?, NSError?) -> Void
@@ -31,76 +30,30 @@ protocol JSONDecodable {
 
 protocol Endpoint {
 	
-	var url: String? { get }
-	var path: String? { get }
-	var params: [String: AnyObject]? { get }
+	var baseURL: NSURL { get }
+	var path: String { get }
+	var request: NSURLRequest { get }
 }
 
-extension Endpoint {
-	
-	var queryComponents: [NSURLQueryItem]? {
-		
-		var components = [NSURLQueryItem]()
-		
-		if let params = params {
-			
-			for (key, value) in params {
-				
-				let queryItem = NSURLQueryItem(name: key, value: "\(value)")
-				
-				components.append(queryItem)
-			}
-		}
-		
-		if components.count > 0 {
-			
-			return components
-			
-		} else {
-			
-			return nil
-		}
-	}
-	
-	var request: NSURLRequest? {
-		
-		guard let url = url, path = path else {
-		
-			return nil
-		}
-		
-		let components = NSURLComponents(string: url)
-		components?.path = path
-		components?.queryItems = queryComponents
-		
-		if let fullUrl = components?.URL {
-			
-			return NSURLRequest(URL: fullUrl)
-			
-		} else {
-			
-			return nil
-		}
-	}
-}
+
 
 protocol APIClient {
 	
 	var configuration: NSURLSessionConfiguration { get }
 	var session: NSURLSession { get }
 	
-	func jsonTaskWithRequest(request: NSURLRequest, completion: JSONTaskCompletion) -> JSONTask
+	func JSONTaskWith(request: NSURLRequest, completion: ([String: AnyObject]?, NSHTTPURLResponse?, NSError?) -> Void) -> NSURLSessionDataTask
 	
-	func fetch<T: JSONDecodable>(request: NSURLRequest, parse: JSON -> T?, completion: APIResult<T> -> Void)
+	func fetch<T: JSONDecodable>(request: NSURLRequest, parse: [String: AnyObject] -> T?, completion: APIResult<T> -> Void)
 }
 
 extension APIClient {
 	
-	func jsonTaskWithRequest(request: NSURLRequest, completion: JSONTaskCompletion) -> JSONTask {
+	func JSONTaskWith(request: NSURLRequest, completion: ([String: AnyObject]?, NSHTTPURLResponse?, NSError?) -> Void) -> NSURLSessionDataTask {
 		
-		let task = session.dataTaskWithRequest(request) { dataCandidate, responseCandidate, error in
+		let task = session.dataTaskWithRequest(request) { data, response, error in
 			
-			guard let hTTPresponse = responseCandidate as? NSHTTPURLResponse else {
+			guard let httpResponse = response as? NSHTTPURLResponse else {
 				
 				let userInfo = [
 					NSLocalizedDescriptionKey: NSLocalizedString("Missing HTTP Response", comment: "")
@@ -108,14 +61,15 @@ extension APIClient {
 				
 				let error = NSError(domain: ANTNetworkingErrorDomain, code: MissingHTTPResponseError, userInfo: userInfo)
 				
+				//no data, no response, error occured
 				completion(nil, nil, error)
 				
-				return //returns from the current scope which in this case is the completion of session.dataTaskWithRequest
+				return
 			}
 			
-			if let dataCandidate = dataCandidate {
+			if let dataCandidate = data {
 				
-				switch hTTPresponse.statusCode {
+				switch httpResponse.statusCode {
 					
 					case 200:
 						
@@ -123,31 +77,28 @@ extension APIClient {
 							
 							let json = try NSJSONSerialization.JSONObjectWithData(dataCandidate, options: []) as? [String: AnyObject]
 							
-							completion(json, hTTPresponse, error)
+							completion(json, httpResponse, error)
 							
 						} catch let err as NSError {
 							
-							completion(nil, hTTPresponse, err)
+							completion(nil, httpResponse, err)
 						}
 						
-						default: print("Received HTTP response: \(hTTPresponse.statusCode).")
-						
-					}
-				
+					default: print("Received HTTP response: \(httpResponse.statusCode).")
+				}
 				
 			} else {
 				
-				completion(nil, hTTPresponse, error)
+				completion(nil, httpResponse, error)
 			}
-			
 		}
 		
 		return task
 	}
 	
-	func fetch<T>(request: NSURLRequest, parse: JSON -> T?, completion: APIResult<T> -> Void) {
+	func fetch<T>(request: NSURLRequest, parse: [String: AnyObject] -> T?, completion: APIResult<T> -> Void) {
 		
-		let task = jsonTaskWithRequest(request) { json, response, error in
+		let task = JSONTaskWith(request) { json, response, error in
 			
 			dispatch_async(dispatch_get_main_queue()) {
 				
@@ -159,7 +110,7 @@ extension APIClient {
 						
 					} else {
 						
-						let abnormalError = NSError(domain: ANTNetworkingErrorDomain, code: MissingErrorError, userInfo: nil)
+						let abnormalError = NSError(domain: ANTNetworkingErrorDomain, code: AbnormalError, userInfo: nil)
 						
 						completion(.Failure(abnormalError))
 					}
@@ -176,51 +127,6 @@ extension APIClient {
 					let unexpectedError = NSError(domain: ANTNetworkingErrorDomain, code: UnexpectedResponseError, userInfo: nil)
 					
 					completion(.Failure(unexpectedError))
-					
-				}
-			}
-		}
-		
-		task.resume()
-	}
-	
-	func fetch<T: JSONDecodable>(endpoint: Endpoint, parse: JSON -> [T]?, completion: APIResult<[T]> -> Void) {
-		
-		guard let request = endpoint.request else {
-			
-			return
-		}
-		
-		let task = jsonTaskWithRequest(request) { json, response, error in
-			
-			dispatch_async(dispatch_get_main_queue()) {
-				
-				guard let json = json else {
-					
-					if let normalError = error {
-						
-						completion(.Failure(normalError))
-						
-					} else {
-						
-						let abnormalError = NSError(domain: ANTNetworkingErrorDomain, code: MissingErrorError, userInfo: nil)
-						
-						completion(.Failure(abnormalError))
-					}
-					
-					return
-				}
-				
-				if let value = parse(json) {
-					
-					completion(.Success(value))
-					
-				} else {
-					
-					let unexpectedError = NSError(domain: ANTNetworkingErrorDomain, code: UnexpectedResponseError, userInfo: nil)
-					
-					completion(.Failure(unexpectedError))
-					
 				}
 			}
 		}
